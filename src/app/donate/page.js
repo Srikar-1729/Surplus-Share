7e7c4cee8d6d20cffcacb39001171b694e1ba3ee
Srikar-1729 <srikar.s1729@gmail.com>
2026-03-12 12:38:54 +0530

    punctuation2 error corrected

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -3,158 +3,165 @@ import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Input } from "@/components/ui/input"
-import {supabase} from "../lib/supabaseClient"
-import { useForm } from "react-hook-form";
-import { useSession } from 'next-auth/react';
+import { supabase } from "../lib/supabaseClient"
+import { useForm } from "react-hook-form"
+import { useSession } from 'next-auth/react'
 import { useState } from "react"
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'
 
-export default function Donate(){
-    const [isClicked,setClicked] = useState(false);
-    const router = new useRouter();
-    const { data: session,status } = useSession();
-    // console.log(data);
-    const { register, handleSubmit, setValue } = useForm();
-    const handleClick = ()=>{
-        setClicked(true);
-        setTimeout(() => setClicked(false), 100);
+export default function Donate() {
+
+    const [isClicked, setClicked] = useState(false)
+    const router = useRouter()
+
+    const { data: session, status } = useSession()
+
+    const { register, handleSubmit, setValue } = useForm()
+
+    const handleClick = () => {
+        setClicked(true)
+        setTimeout(() => setClicked(false), 100)
     }
-    const handleDonation = async(d) =>{
-        console.log(d);
-        if (status === "loading") {
-        return <p>Loading...</p>;
-        }
 
-        if (!session) {
-        return <p>Not logged in</p>;
-        }
-        
-        const { data,error } = await supabase.from("donations").insert([{
-            donor_id : session.user.id,
-            food_type : d.food_type,
-            food_description : d.food_description,
-            can_deliver : d.can_deliver==="yes"?true:false,
-            expiry : d.expiry,
-            quantity : d.quantity,
-            location : d.location,
-            additional_info : d.additional_info,
-            status : "active"
-    }]);
-           
-            if (error) {
+    if (status === "loading") return <p>{"Loading..."}</p>
+    if (!session) return <p>{"Not logged in"}</p>
+
+    const handleDonation = async (d) => {
+
+        const { data, error } = await supabase
+            .from("donations")
+            .insert([
+                {
+                    donor_id: session.user.id,
+                    food_type: d.food_type,
+                    food_description: d.food_description,
+                    can_deliver: d.can_deliver === "Yes",
+                    expiry: d.expiry,
+                    quantity: d.quantity,
+                    location: d.location,
+                    additional_info: d.additional_info,
+                    status: "active"
+                }
+            ])
+
+        if (error) {
             console.log(error.message)
-            }
-            else{
-                router.push("../search")
-            }
-        
+        } else {
+            router.push("../search")
+        }
     }
-    return(
+
+    return (
         <div className="w-full h-full flex justify-center pt-14">
-    <form
-        className="h-fit p-6 bg-white w-1/2 shadow-lg flex flex-col gap-6"
-        onSubmit={handleSubmit(handleDonation)}
-    >
-        <h1 className="text-center text-5xl font-semibold">
-            {"Donate Food"}
-        </h1>
 
-        {/* <p className="text-3xl font-medium">Details</p> */}
+            <form
+                className="h-fit p-6 bg-white w-1/2 shadow-lg flex flex-col gap-6"
+                onSubmit={handleSubmit(handleDonation)}
+            >
 
-        <div className="flex justify-between">
-            <div>
-                <p className="text-2xl mb-2">{"Food type"}</p>
+                <h1 className="text-center text-5xl font-semibold">
+                    {"Donate Food"}
+                </h1>
 
-                <RadioGroup
-                    defaultValue="none"
-                    onValueChange={(value) => setValue("food_type", value)}
-                >
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="Veg" id="option-one" />
-                        <Label htmlFor="option-one">{"Veg"}</Label>
+                <div className="flex justify-between">
+
+                    <div>
+                        <p className="text-2xl mb-2">{"Food type"}</p>
+
+                        <RadioGroup
+                            defaultValue="none"
+                            onValueChange={(value) => setValue("food_type", value)}
+                        >
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="Veg" id="option-one" />
+                                <Label htmlFor="option-one">{"Veg"}</Label>
+                            </div>
+
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="Non-Veg" id="option-two" />
+                                <Label htmlFor="option-two">{"Non-Veg"}</Label>
+                            </div>
+                        </RadioGroup>
                     </div>
 
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="Non-Veg" id="option-two" />
-                        <Label htmlFor="option-two">{"Non-Veg"}</Label>
+                    <div>
+                        <p className="text-2xl mb-2">{"No.of Servings"}</p>
+
+                        <Input
+                            id="quantity"
+                            type="number"
+                            min={1}
+                            step={1}
+                            defaultValue={1}
+                            className="w-39"
+                            {...register("quantity")}
+                        />
                     </div>
-                </RadioGroup>
-            </div>
 
-            <div>
-                <p className="text-2xl mb-2">{"No.of Servings"}</p>
+                    <div>
+                        <p className="text-2xl mb-2">{"Can you deliver?"}</p>
 
-                <div className="flex flex-col space-y-2">
+                        <RadioGroup
+                            defaultValue="none"
+                            onValueChange={(value) => setValue("can_deliver", value)}
+                        >
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="Yes" id="option-1" />
+                                <Label htmlFor="option-1">{"Yes"}</Label>
+                            </div>
+
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="No" id="option-2" />
+                                <Label htmlFor="option-2">{"No"}</Label>
+                            </div>
+
+                            <input type="hidden" {...register("can_deliver")} />
+                        </RadioGroup>
+                    </div>
+
+                </div>
+
+                <div>
+                    <p className="text-2xl mb-2">{"Food Items"}</p>
+                    <Textarea {...register("food_description")} />
+                </div>
+
+                <div>
+                    <p className="text-2xl mb-2">{"Safe To Consume Within"}</p>
+
                     <Input
-                        id="quantity"
-                        type="number"
-                        min={1}
-                        step={1}
-                        defaultValue={1}
-                        className="w-39"
-                        {...register("quantity")}
+                        id="expiry"
+                        type="datetime-local"
+                        className="w-fit"
+                        {...register("expiry")}
                     />
                 </div>
-            </div>
 
-            <div>
-                <p className="text-2xl mb-2">{"Can you deliver?"}</p>
+                <div>
+                    <p className="text-2xl mb-2">{"Pickup Address"}</p>
+                    <Input id="address" type="text" {...register("location")} />
+                </div>
 
-                <RadioGroup
-                    defaultValue="none"
-                    onValueChange={(value) => setValue("can_deliver", value)}
-                >
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="Yes" id="option-1" />
-                        <Label htmlFor="option-1">{"Yes"}</Label>
-                    </div>
+                <div>
+                    <p className="text-2xl mb-2">{"Additional Info (Optional)"}</p>
+                    <Textarea {...register("additional_info")} />
+                </div>
 
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="No" id="option-2" />
-                        <Label htmlFor="option-2">{"No"}</Label>
-                    </div>
+                <div className="flex justify-center">
 
-                    <input type="hidden" {...register("can_deliver")} />
-                </RadioGroup>
-            </div>
+                    <button
+                        onClick={handleClick}
+                        className={`border-2 border-black text-white rounded bg-green-500 text-2xl px-7 py-2 ${
+                            isClicked ? "translate-y-1" : ""
+                        }`}
+                    >
+                        {"Submit"}
+                    </button>
+
+                </div>
+
+            </form>
+
         </div>
-
-        <div>
-            <p className="text-2xl mb-2">{"Food Items"}</p>
-            <Textarea name="food_items" {...register("food_description")} />
-        </div>
-
-        <div>
-            <p className="text-2xl mb-2">{"Safe To Consume Within"}</p>
-            <Input
-                id="expiry"
-                type="datetime-local"
-                name="expiry"
-                className="w-fit"
-                {...register("expiry")}
-            />
-        </div>
-
-        <div>
-            <p className="text-2xl mb-2">{"Pickup Address"}</p>
-            <Input id="address" type="text" name="address" {...register("location")} />
-        </div>
-
-        <div>
-            <p className="text-2xl mb-2">{"Additional Info (Optional)"}</p>
-            <Textarea name="add_info" {...register("additional_info")} />
-        </div>
-
-        <div className="flex justify-center">
-            <button
-                onClick={handleClick}
-                className={`border-2 border-black text-white rounded bg-green-500 text-2xl px-7 py-2 ${
-                    isClicked ? "translate-y-1" : ""
-                }`}
-            >
-                {"Submit"}
-            </button>
-        </div>
-    </form>
-</div>);
+    )
 }
